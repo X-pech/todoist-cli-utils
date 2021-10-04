24f1cb976984109bdf9a4477c62b6e15c47419f7
X-pech <lisgml@gmail.com>
2021-10-04 18:09:57 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todoist-cli-utils
+
+Some shell script to make use of [todoist cli client](https://github.com/sachaos/todoist) more convinient for personal use
---
+++ todoist-cli-utils.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function todoist-today() {
+    todoist sync
+    todoist l -f today
+}
+
+function todoist-soon() {
+    todoist sync
+    todoist l -f '(today) | (tomorrow) | (overdue)'
+}
+
+function todoist-work() {
+    todoist sync
+    todoist l -f '#Работа'
+}
+
+function todoist-c() {
+    todoist close $($@ | peco | awk ' {print $1}')
+    todoist sync
+}
+
+function todoist-d() {
+    todoist d $($@ | peco | awk ' {print $1}')
+    todoist sync
+}
+
+
+function todoist-add() {
+    read -r -p "Введите date или date string: " datestr
+    PROJ=$(todoist projects | peco | awk '{ print $1 }')
+    read -n 1 -p "Нужна метка? [Yy/Nn/Дд/Нн]:" label
+    case $label in
+        [YyДд]* ) label="-L "$(todoist labels | peco | awk 'BEGIN {ORS=","} { print $1 }');;
+        [NnНн]* ) label="";;
+        * ) echo "Ясно, ты ебан"; label="";;
+    esac
+    read -r -n1 -p "Приоритет? [1-4]:" prior
+
+    if [[ $prior =~ ^[0-9]+ && prior -ge 1  && prior -ge 4 ]]; then
+        prior="-p "$prior
+    else
+        prior=""
+    fi
+
+    todoist add "$1" -d "$datestr" $prior $label -P "$PROJ"
+    todoist sync
+}
